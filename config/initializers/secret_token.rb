e2a247d825c1ffbbbe10b6f420c51ed0c496ca17
Andrew Elliott <contact@awegroup.com>
2013-05-18 06:45:41 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackDay2013::Application.config.secret_token = '4f5c84c3e2ef7babc2e65577810171bca85d3202fe523bd2779a38a9053b2e801c812c10bf1141adaca7e2251a3901e0540644ec8b9827bef4069891bf60c3a7'
